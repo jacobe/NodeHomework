47454eae68d068748f05a70acc7bf92d80e9d71c
Jacob Eisenberg <jei@trifork.com>
2014-06-16 22:27:12 +0200

    Added server-side logic to save individual weeks.

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var moment = require('moment');
 
 var app = express();
 app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
-var registrations = [];
+// This simulates a database, keeping data in-memory for the app lifetime.
+var registrations = {};
 
 app.get('/', function(req, res) {
 	res.sendfile('html/index.html');
 });
 
 app.get('/api/registrations', function(req, res) {
-	res.send(registrations);
+	var date = moment(req.query.date).startOf('isoweek').toISOString();
+	res.send(registrations[date] || []);
 });
 
 app.post('/api/registrations', function(req, res) {
-	registrations = req.body.registrations;
+	var startDate = req.body.startDate;
+	registrations[startDate] = req.body.registrations;
 	res.end();
 });
 
